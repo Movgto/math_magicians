f480fabf3e30cedbe676b15de6f2528dd303529a
GUMARO MONROY VAZQUEZ <gumaro.monroyv@uanl.edu.mx>
2023-08-15 13:56:45 -0600

    Revert "Add calculator components and styles"
    This reverts commit 1c2b576795a2b6fa0753ed9638f16f1b8255e43e.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import './App.css';
-import Calculator from './components/Calculator';
 
 function App() {
   return (
-    <div className="App"><Calculator screen="0" /></div>
+    <div className="App">Math Magician</div>
   );
 }
 
